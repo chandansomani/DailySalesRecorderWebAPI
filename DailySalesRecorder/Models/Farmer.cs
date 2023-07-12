6778bb8177dfd37ce3ee392cd83dfda36e68818b
Chandan Somani <chandansomani@gmail.com>
2023-07-12 18:19:20 +0530

    Added Defualt values to Model Properties

--- DailySalesRecorder/Models/Farmer.cs
+++ DailySalesRecorder/Models/Farmer.cs
@@ -4,14 +4,14 @@ namespace DailySalesRecorder.Models
 {
     public class Farmer
     {
-        [Key] public int FarmerId { get; set; }
-        public string FarmerName { get; set; }
-        public string FarmerPlace { get; set; }
-        public ulong MobileNo { get; set; }
-        public ulong Aadhaar { get; set; }
-        public int CreatedBy { get; set; }
-        public int UpdatedBy { get; set; }
-        public DateTime CreatedDate { get; set; }
-        public DateTime UpdatedDate { get; set;}
+        [Key] public int FarmerId { get; set; } = 0;
+        public string FarmerName { get; set; } = string.Empty;
+        public string FarmerPlace { get; set; } = string.Empty;
+        public ulong MobileNo { get; set; } = 0;
+        public ulong Aadhaar { get; set; } = 0;
+        public int CreatedBy { get; set; } = 0;
+        public int UpdatedBy { get; set; } = 0;
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+        public DateTime UpdatedDate { get; set; } = DateTime.Now;
     }
 }
--- DailySalesRecorder/Models/Invoice.cs
+++ DailySalesRecorder/Models/Invoice.cs
@@ -4,17 +4,17 @@ namespace DailySalesRecorder.Models
 {
     public class Invoice
     {
-        [Key]public int InvoiceID { get; set; }
-        public InvoiceType InvoiceType { get; set; }
-        public DateTime Date { get; set; }
-        public int FarmerId { get; set; }
-        public decimal Amount { get; set; }
-        public decimal CreditAmt { get; set; }
-        public decimal CashAmt { get; set; }
-        public decimal UPIAmt { get; set; }
-        public  int CreatedBy { get; set; }
-        public int UpdatedBy { get; set; }
-        public DateTime CreatedDate { get; set; }
-        public DateTime UpdatedDate { get; set; }
+        [Key] public int InvoiceID { get; set; } = 0;
+        public InvoiceType InvoiceType { get; set; } = InvoiceType.SEED;
+        public DateTime Date { get; set; } = DateTime.Now;
+        public Farmer FarmerId { get; set; }
+        public decimal Amount { get; set; } = decimal.Zero;
+        public decimal CreditAmt { get; set; } = decimal.Zero;
+        public decimal CashAmt { get; set; } = decimal.Zero;
+        public decimal UPIAmt { get; set; } = decimal.Zero;
+        public int CreatedBy { get; set; } = 0;
+        public int UpdatedBy { get; set; } = 0;
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+        public DateTime UpdatedDate { get; set; } = DateTime.Now;
     }
 }
