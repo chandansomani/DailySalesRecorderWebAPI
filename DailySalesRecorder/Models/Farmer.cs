cb3dfade8a6bda06adeaf0a5cb133b2c95cf5cf6
Chandan Somani <chandansomani@gmail.com>
2023-07-12 12:13:04 +0530

    Farmer Model Created

---
+++ DailySalesRecorder/Models/Farmer.cs
@@ -0,0 +1,15 @@
+﻿namespace DailySalesRecorder.Models
+{
+    public class Farmer
+    {
+        int FarmerId { get; set; }
+        public string FarmerName { get; set; }
+        public string FarmerPlace { get; set; }
+        public ulong MobileNo { get; set; }
+        public ulong Aadhaar { get; set; }
+        public int CreatedBy { get; set; }
+        public int UpdatedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set;}
+    }
+}
